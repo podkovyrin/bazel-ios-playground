58ccf71c763b7b8d6a8bfef85d0fc0cd4ec44df1
Andrew Podkovyrin <podkovyrin@gmail.com>
2022-10-17 07:11:47 +0200

    init

---
+++ bzl-playground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bzl-playground
+//
+//  Created by Andrew Podkovyrin on 17/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
